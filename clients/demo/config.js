4f01f5282675f3fd914d9303c72ee02a687f5b9c
PIXORA24 <pixora24@gmail.com>
2026-01-16 12:17:16 +0530

    Create config.js

---
+++ clients/demo/config.js
@@ -0,0 +1,20 @@
+window.INVITE_DATA = {
+  events: {
+    wedding: {
+      label: "Wedding",
+      enabled: true,
+      dateTimeISO: "2026-06-28T16:00:00+05:30",
+      venue: "Bangalore",
+      mapLink: "https://maps.app.goo.gl/demo",
+      path: "media/wedding/"
+    },
+    reception: {
+      label: "Reception",
+      enabled: true,
+      dateTimeISO: "2026-06-30T19:00:00+05:30",
+      venue: "Bangalore",
+      mapLink: "https://maps.app.goo.gl/demo",
+      path: "media/reception/"
+    }
+  }
+};
