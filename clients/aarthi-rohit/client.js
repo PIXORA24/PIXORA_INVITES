5cae05ebcfae591616dd0b275337e5b2f3c6b673
PIXORA24 <pixora24@gmail.com>
2026-01-16 12:48:55 +0530

    Create client.js

---
+++ clients/aarthi-rohit/client.js
@@ -0,0 +1,26 @@
+window.INVITE_DATA = {
+  couple: {
+    bride: "Aarthi Verma",
+    groom: "Rohit Dikshit"
+  },
+
+  events: {
+    wedding: {
+      label: "Wedding",
+      enabled: true,
+      dateTimeISO: "2026-06-28T16:00:00+05:30",
+      venue: "Royal Orchid Banquet Hall, Delhi",
+      mapLink: "https://maps.app.goo.gl/7G8gX8R8YpQ8kT7x8",
+      path: "clients/aarthi-rohit/media/wedding/"
+    },
+
+    reception: {
+      label: "Reception",
+      enabled: true,
+      dateTimeISO: "2026-06-30T19:00:00+05:30",
+      venue: "The Leela Ambience Convention Hotel, Delhi",
+      mapLink: "https://maps.app.goo.gl/1xYzQ8m8W9D9n8sP6",
+      path: "clients/aarthi-rohit/media/reception/"
+    }
+  }
+};
